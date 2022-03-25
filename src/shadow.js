eb738c934c5bab67ee3403477eebead2bd093ace
Jalal azimi <jalalazimi@gmail.com>
2022-03-25 14:30:12 +0000

    lights

--- src/shadow.js
+++ src/shadow.js
@@ -14,29 +14,24 @@ function init() {
 
   if (window.enbaledFog) scene.fog = new THREE.FogExp2(0xffffff, 0.2);
 
-  const plane = getPlane(30);
-  const boxGrid = getBoxGrid(10, 0.5);
-  const ambientLight = getAmbientLight(1);
-  const light = getDirectionLight(1);
+  const plane = getPlane(20);
+  const box = getBox(1, 1, 1);
+  const pointLight = getPointLight(1);
   const sphere = getSphere(0.05);
-  const helper = new THREE.CameraHelper(light.shadow.camera);
-  light.position.y = 8;
 
+  pointLight.position.y = 2;
+
+  box.position.y = box.geometry.parameters.height / 2;
   plane.name = "plane-1";
   plane.rotation.x = Math.PI / 2;
 
-  light.add(sphere);
-  scene.add(boxGrid);
+  pointLight.add(sphere);
+  scene.add(box);
   scene.add(plane);
-  scene.add(light);
-  scene.add(helper);
-  scene.add(ambientLight);
+  scene.add(pointLight);
 
-  gui.add(light, "intensity", 0, 10);
-  gui.add(light.position, "y", 0, 10);
-  gui.add(light.position, "x", 0, 10);
-  gui.add(light.position, "z", 0, 10);
-  // gui.add(light, "penumbra", 0, 1);
+  gui.add(pointLight, "intensity", 0, 10);
+  gui.add(pointLight.position, "y", 0, 10);
 
   const camera = new THREE.PerspectiveCamera(
     45,
@@ -72,31 +67,6 @@ function getPointLight(intensity) {
   return light;
 }
 
-function getSpotLight(intensity) {
-  const light = new THREE.SpotLight("#ffffff", intensity);
-  light.castShadow = true;
-  light.shadow.bias = 0.001;
-  light.shadow.mapSize.width = 2048;
-  light.shadow.mapSize.height = 2048;
-  return light;
-}
-
-function getDirectionLight(intensity) {
-  const light = new THREE.DirectionalLight("#ffffff", intensity);
-  light.castShadow = true;
-
-  light.shadow.camera.left = -10;
-  light.shadow.camera.top = -10;
-  light.shadow.camera.right = -10;
-  light.shadow.camera.bottom = -10;
-
-  return light;
-}
-
-function getAmbientLight(intensity) {
-  const light = new THREE.AmbientLight("rgb(10, 30, 50)", intensity);
-  return light;
-}
 function getBox(w, h, d) {
   const geometry = new THREE.BoxGeometry(w, h, d);
   const material = new THREE.MeshPhongMaterial({
@@ -105,29 +75,6 @@ function getBox(w, h, d) {
   const mesh = new THREE.Mesh(geometry, material);
   mesh.castShadow = true;
   return mesh;
-}
-
-function getBoxGrid(count, gap) {
-  const group = new THREE.Group();
-  gap = gap + 1;
-  for (let i = 0; i <= count; i++) {
-    const box = getBox(1, 1, 1);
-    box.position.x = i * gap;
-    box.position.y = box.geometry.parameters.height / 2;
-    group.add(box);
-    for (let j = 0; j <= count; j++) {
-      const box = getBox(1, 1, 1);
-      box.position.x = i * gap;
-      box.position.y = box.geometry.parameters.height / 2;
-      box.position.z = j * gap;
-      group.add(box);
-    }
-  }
-
-  group.position.x = -(gap * (count - 1)) / 2;
-  group.position.z = -(gap * (count - 1)) / 2;
-
-  return group;
 }
 
 function getSphere(size) {
